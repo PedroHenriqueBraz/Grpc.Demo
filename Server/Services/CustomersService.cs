0684a782e48f01bffc01d323d8c7637fa0a24470
Pedro H Braz <braz.hpedro@gmail.com>
2019-10-20 21:02:51 -0300

    add novos services

---
+++ Server/Services/CustomersService.cs
@@ -0,0 +1,65 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public class CustomersService : Customers.CustomersBase
+    {
+        public ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfor(CustomerModelRequest request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.Name = "tortoruga";
+                output.Age = 1;
+                output.IsActive = true;
+            }
+            else
+            {
+                output.Name = "teste";
+                output.Age = 0;
+                output.IsActive = false;
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetCustomers(CustomersRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> output = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    Name = "tortoruga",
+                    Age = 1,
+                    IsActive = true,
+                },
+                new CustomerModel
+                {
+                    Name = "teste",
+                    Age = 0,
+                    IsActive = false,
+
+                }
+            };
+
+            foreach (var custumer in output)
+            {
+                await Task.Delay(2000);
+                await responseStream.WriteAsync(custumer);
+            }
+        }
+    }
+} 
--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Server;
+using System;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.ReadLine(); 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var clientGreet = new Greeter.GreeterClient(channel);
+            var requestGreet = new HelloRequest() { Name = "Pedro" };
+            var reply = await clientGreet.SayHelloAsync(requestGreet);
+            Console.WriteLine("greeter: " + reply.Message);
+
+            var clientCustomer = new Customers.CustomersClient(channel);
+
+            var request = new CustomerModelRequest { UserId = 1 };
+            var result = await clientCustomer.GetCustomerInforAsync(request);
+            Console.WriteLine($"Customer: {result.Name} {result.Age} {result.IsActive}");
+
+            using (var call = clientCustomer.GetCustomers(new CustomersRequest()))
+            {
+                Console.WriteLine("lista customers");
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var customer = call.ResponseStream.Current;
+                    Console.WriteLine($"{customer.Name} {customer.Age} {customer.IsActive}");
+                }
+            }
+                Console.ReadLine();  
         }
     }
 }
